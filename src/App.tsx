73b3ccd305dc9cd4cd1fad7196da09c655a3cef1
Will Bentley <38542573+williamkbentley@users.noreply.github.com>
2022-02-07 10:11:42 -0500

    with fetch only

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserAuthorizationClient } from '@itwin/browser-authorization';
+import { AccessToken } from '@itwin/core-bentley';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [accessToken, setAccessToken] = useState<AccessToken>();
+  const [projectData, setProjectData] = useState<any[]>([]);
+
+  const authClient = useMemo(
+    () =>
+      new BrowserAuthorizationClient({
+        scope: process.env.IMJS_AUTH_CLIENT_SCOPES ?? "",
+        clientId: process.env.IMJS_AUTH_CLIENT_CLIENT_ID ?? "",
+        redirectUri: process.env.IMJS_AUTH_CLIENT_REDIRECT_URI ?? "",
+        postSignoutRedirectUri: process.env.IMJS_AUTH_CLIENT_LOGOUT_URI,
+        responseType: "code",
+        authority: process.env.IMJS_AUTH_AUTHORITY,
+      }),
+    []
+  );
+
+  const getProjects = useCallback(async () => {
+    if (accessToken) {
+      const response = await fetch('https://api.bentley.com/projects/',
+        {
+          method: 'GET',
+          headers: {
+            Authorization: accessToken,
+          }
+        });
+      if (response.ok) {
+        const responseBody = await response.json();
+        setProjectData(responseBody.projects);
+      }
+    };
+  }, [accessToken]);
+
+  const showImodelInfo = async (projectId: string) => {
+    if (accessToken) {
+      const response = await fetch(`https://api.bentley.com/imodels/?projectId=${projectId}`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: accessToken,
+          }
+        });
+      if (response.ok) {
+        const respBody = await response.json();
+        console.log(respBody);
+      }
+    }
+  };
+
+  useEffect(() => {
+    void getProjects();
+  }, [accessToken, getProjects]);
+
+  const login = useCallback(async () => {
+    try {
+      await authClient.signInSilent();
+    } catch {
+      await authClient.signIn();
+    }
+
+    const at = await authClient.getAccessToken();
+    setAccessToken(at);
+  }, [authClient]);
+
+  useEffect(() => {
+    void login();
+  }, [login]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App-header">
+      {!accessToken && (
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          hello world, you're not signed in
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      )}
+      <p>you're signed in</p>
+      <div>
+        <table className='project-table'>
+          <tbody>
+
+            <tr>
+              {projectData.length > 0 &&
+                Object.keys(projectData[0]).map((k: any) => {
+                  return (
+                    <th>
+                      {k}
+                    </th>
+                  )
+                })
+              }
+            </tr>
+            {projectData.length > 0 &&
+              Object.keys(projectData).map((k: any) => {
+                return (<tr key={k} onClick={() => { showImodelInfo(projectData[k]['id']); }}>{
+                  Object.keys(projectData[k]).map((k2: any) => {
+                    return (<td>{projectData[k][k2]}</td>)
+                  })}
+                </tr>)
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    </div >
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserAuthorizationCallbackHandler } from "@itwin/browser-authorization";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (!process.env.IMJS_AUTH_CLIENT_REDIRECT_URI) {
+  throw new Error(
+    "Please add a valid redirect URI to the .env file and restart the application."
+  );
+}
+const redirectUrl = new URL(process.env.IMJS_AUTH_CLIENT_REDIRECT_URI);
+if (redirectUrl.pathname === window.location.pathname) {
+  BrowserAuthorizationCallbackHandler.handleSigninCallback(
+    redirectUrl.toString()
+  ).catch(console.error);
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
