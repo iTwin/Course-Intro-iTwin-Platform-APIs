64856396b917a7f191185989c60cc7bfd4f01ae9
williamkbentley <38542573+williamkbentley@users.noreply.github.com>
2022-07-05 13:20:58 -0400

    Prep for public. Code cleanup. Copyrights. (#7)
    * Prep for public. Code cleanup. Copyrights.
    
    * Update package lock
    
    * Update package-lock with node 16 support
    
    * Update again

--- src/IModelThumbnail.tsx
+++ src/IModelThumbnail.tsx
@@ -1,3 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *
+ * This code is for demonstration purposes and should not be considered production ready.
+ *--------------------------------------------------------------------------------------------*/
+
 import { AccessToken } from "@itwin/core-bentley";
 import { MinimalIModel } from "@itwin/imodels-client-management";
 import { useState, useCallback, useEffect } from "react";
